6ba1c52bcaf7dd3570f9cd5ddb56243fa1951fda
bharatjangid760-dt <bharatjangid760@gmail.com>
2026-01-03 23:12:44 +0530

    Update config handling and environment variables

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-token=os.getenv("BOT_TOKEN")
-url = os.getenv("COIN_GECKO_URL")
-currencies = os.getenv("CURRENCIES")
+token=os.getenv("8265179925:AAFw6PxMbk_HiVeeATYiQU_1O9jZ72tZ7m4")
+url = os.getenv("https://api.coingecko.com/api/v3/simple/price")
+currencies = os.getenv("usd, inr")
